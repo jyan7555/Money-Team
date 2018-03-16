206fb2c4020d270e17caa2907a77746eb6dc5517
Student <student@techs-imac-224.78b430.nycboe.org>
2018-03-16 10:57:13 -0400

    rtrtet

--- Money Team/GameViewController.swift
+++ Money Team/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("hi")
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
